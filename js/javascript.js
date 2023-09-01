11a43ff524bdf9c320ba9338eabd6ec2a4aed9bd
CarlosDosGit <chsilva1419@gmail.com>
2023-08-31 23:11:46 -0300

    V4.0

--- js/javascript.js
+++ js/javascript.js
@@ -1,110 +1,103 @@
-// JS - Carrossel
-const controls = 
-document.querySelectorAll('.control');
-let currentItem = 0;
-const items =
-document.querySelectorAll('.item');
-const maxItems = items.length;
+//Cursos
+function Filosofia(){
+    Perfil = "Pessoas com perfil crítico, observador e questionador são as mais indicadas para estudar Filosofia. É preciso que o estudante goste de ler, pois todo o conteúdo é composto por textos densos, com o estudo de linhas de pensamento de filósofos importantes na História.";
+    Mercado = "As principais áreas na qual um graduado em filosofia pode trabalhar são: Docência, Pesquisa e carreira acadêmica, Colunista e crítico em veículos de comunicação, Setor público e ONGs, Mercado editorial. O Filosofo ganha no Brasil, em média, de <b class='CkJ'>R$2000,00</b> a <b class='CkJ'>R$ 6.500,00.</b>";
+    Curso = "Em uma faculdade de filosofia você estudará os princípios fundamentais da vida e da existência humana. Normalmente, cursos que possuem graduações em licenciatura ou bacharelado o aluno pode se formar em 4 anos, ou seja, 8 semestres. As faculdades oferecem um curso de graduação com disciplinas que abrangem diferentes ramos da filosofia, como filosofia moral, política, social, religiosa, epistemológica, existencial etc.";
+    Titulo = "Filosofia";
+    imagem = "<img src='img/Filosofia.png' class='img-fluid' id='tm'>";
 
-controls.forEach(control => {
-    control.addEventListener('click', () => {
-        const isLeft = control.classList.contains('arrow-left');
-
-
-        if (isLeft) {
-            currentItem -= 1;
-        }else {
-            currentItem += 1;
-        }
-
-        if (currentItem >= maxItems){
-            currentItem = 0;
-        }
-        if (currentItem < 0){
-            currentItem = maxItems - 1;
-        }
-
-        items.forEach((item) => item.classList.remove('current-item'));
-
-        items[currentItem].scrollIntoView({inline: 'center', behavior: 'smooth'});
-
-        items[currentItem].classList.add('current-item');
-    })
-})
-
-
-function button(){
-    const el = document.querySelector('.arrow-left,.arrow-right');
-    el.style.cssText =
-    'height: 92vh;' +
-    'opacity: 2';
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
 
 }
-function buttonS(){
-    const el = document.querySelector('.arrow-left,.arrow-right');
-    el.style.cssText =
-    'height: 90vh;' +
-    'opacity: 0.5';
+
+function Psicologia(){
+    Perfil = "Para alunos de psicologia é preciso ter as seguintes características: capacidade de empatia, não ter preconceitos, gostar de ler.";
+    Mercado = "Dentre as áreas da psicologia que possuem maiores salários, destacam-se: psicologia clínica, psicologia criminal, psicanálise, psicologia do trabalho entre outras.Tendo isso em mente, sabe-se que o profissional graduado em Psicologia recebe entre <b class='CkJ'>R$1.772,65</b> a <b class='CkJ'>R$7.313,91</b>.";
+    Curso = "Se você deseja investir nesta área, esteja disposto a fazer faculdade de Psicologia por pelo menos cinco anos para garantir sua formação. O curso de graduação em Psicologia tem uma carga horária total mínima de 4.000 horas. As disciplinas do curso são em sua maioria teóricas e abordam as diversas correntes e áreas da Psicologia. O aluno aprende, por exemplo, sobre Psicologia Clínica, Social, Educacional e Organizacional, entre outras.";
+    Titulo = "Psicologia";
+    imagem = "<img src='img/Psicologia.png' class='img-fluid' id='tm'>";
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
 
 }
-function button1(){
-    const el = document.querySelector('.arrow-right');
-    el.style.cssText =
-    'height: 92vh;' +
-    'opacity: 2';
+
+function Direito(){
+    Perfil = "Para alunos de direito é preciso ter as seguintes características: boa oratória e poder de argumentação, vontade de se manter sempre atualizado, gosto por leitura e escrita etc.";
+    Mercado = "Atuando como advogado algumas das áreas de trabalho são: Advocacia, Direito administrativo, Direito ambiental, Direito civil, Direito do consumidor, Direito de propriedade intelectual, Direito digital, Direito penal ou criminal. Podendo receber em média <b class='CkJ'>R$ 4.537,62</b>";
+    Curso = "O curso de direito tem duração média de 5 anos ou 10 semestres. As matérias abrangem diversos aspectos da vida pública e privada e do comportamento humano, como obrigações e responsabilidades, contratos, direito de família, direitos humanos etc.  Normalmente, ao fazer o curso de direito, o estudante tem ao fim do seu curso a opção de atuar como advogado ou seguir carreira jurídica.";
+    Titulo = "Direito";
+    imagem = "<img src='img/Direito.png' class='img-fluid' id='tm'>";
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
 
 }
-function buttonS1(){
-    const el = document.querySelector('.arrow-right');
-    el.style.cssText =
-    'height: 90vh;' +
-    'opacity: 0.5';
+
+function História(){
+    Perfil = "Para os alunos interessados em história, é preciso que desenvolvam um espírito crítico, questionador e uma capacidade de interpretação e reflexão.";
+    Mercado = "Além da área educacional, o historiador também tem a possibilidade de trabalhar na preservação do patrimônio histórico da humanidade e em empresas que envolvem essa área. Estes são os principais setores empresariais que precisam desses profissionais: agências de turismo, emissoras de TV,instituições de ensino profissionalizante. O salário médio de um Historiador é de <b class='CkJ'>R$ 2.659,50</b>";
+    Curso = "A faculdade de História tem como finalidade desenvolver profissionais aptos a disseminar o conhecimento histórico e incentivar o raciocínio crítico, com segura comprovação teórica e prática.  O curso de História tem tempo médio estabelecido de 8 semestres ou 4 anos. Porém, esse tempo pode mudar de acordo com situações específicas.No decorrer do curso, o acadêmico tem a possibilidade de conhecer não somente os acontecimentos relevantes, mas os aspectos ambientais, culturais, econômicos, políticos e sociais dos fatos regionais, nacionais e mundiais.";
+    Titulo = "História";
+    imagem = "<img src='img/Historia.png' class='img-fluid' id='tm'>";
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
+
 }
 
-//JS - Carrossel 2
+function Marketing(){
+    Perfil = "Além de um perfil analítico e inovador, o especialista precisa conhecer o mercado, criar personas e gerenciar conteúdos de valor para seus leads";
+    Mercado = "O profissional da área de Marketing pode atuar em diversas empresas e setores. Os principais campos para exercer a função no Brasil e em todo o mundo, são: agências de propaganda e marketing; setores de Marketing de empresas; consultorias; organizações de todos os tipos e portes e assessoria de celebridades. Valores médios de salário: Valor médio - <b class='CkJ'>1800R$</b>";
+    Curso = "Tecnólogo ou bacharelado, o curso de Marketing busca preparar o aluno para entender e atender ao consumidor. Quem escolhe essa graduação estuda o comportamento dos consumidores, técnicas de negociação, estratégias de venda, posicionamento de marca, competitividade e muito mais. Duração média: no bacharelado, são 4 anos. No tecnológico, 2 anos. Entre as principais disciplinas de estudo estão: Comunicação, Cálculo, Administração, Economia, Sociologia e Inovação.";
+    Titulo = "Marketing";
+    imagem = "<img src='img/Marketing.png' class='img-fluid' id='tm'>";
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
 
-const controlsS = 
-document.querySelectorAll('.controlL');
-let currentItemM = 0;
-const itemsS =
-document.querySelectorAll('.itemM');
-const maxItemsS = items.length;
+}
 
-controlsS.forEach(controlL => {
-    controlL.addEventListener('click', () => {
-        const isLeftT = controlL.classList.contains('arrow-leftT');
-
-
-        if (isLeftT) {
-            currentItemM -= 1;
-        }else {
-            currentItemM += 1;
-        }
-
-        if (currentItemM >= maxItemsS){
-            currentItemM = 0;
-        }
-        if (currentItemM < 0){
-            currentItemM = maxItemsS - 1;
-        }
-
-        itemsS.forEach((itemM) => itemM.classList.remove('current-itemM'));
-
-        itemsS[currentItemM].scrollIntoView({inline: 'center', behavior: 'smooth'});
-
-        itemsS[currentItemM].classList.add('current-itemM');
-    })
-})
-
-
-function Cursos(){
-        Texto = "Esta área de estudo é essencial para entender o que nos move como seres humanos e o que torna nossas vidas significativas, portanto, em uma faculdade de filosofia você estudará os princípios fundamentais da vida e da existência humana.";
-        // imagem = "<img src='img/Foto-Header.png'>";
-        const el = document.querySelector('#egll');
-        el.style.cssText =
-        'background-color: #8059DE;' + 
-        'color: #fffff';
-        document.getElementById("CkJ").innerText = Texto;
-        // document.getElementById("lxsx").innerHTML = imagem;
+function Pedagogia(){
+    Perfil = "Para alunos de pedagogia é preciso ter as seguintes características: gosto por aprender e ensinar, bom comunicação e empatia, vontade de transformar a sociedade, criatividade e curiosidade.";
+    Mercado = "Um profissional formado em pedagogia pode atuar na área da educação formal, da educação infantil, da coordenação e na gestão escolar, como direção de escolas, supervisão educacional e administração escolar etc. Média salarial: A média salarial inicial pode variar de cerca de <b class='CkJ'>R$ 2.000</b> a <b class='CkJ'>R$ 3.500</b>, dependendo da rede de ensino e da carga horária. ";
+    Curso = "O curso de Pedagogia no Brasil é voltado para a formação de profissionais da área da educação, capacitando-os para atuar em diferentes níveis e modalidades de ensino. O curso de Pedagogia possui uma duração média de 4 anos. Existem duas modalidades de graduação em Pedagogia: Licenciatura em Pedagogia: Essa modalidade tem foco na formação de profissionais para atuarem como docentes na Educação Infantil e nos anos iniciais do Ensino Fundamental Bacharelado em Pedagogia: Nessa modalidade, o curso de Pedagogia é voltado para a formação de profissionais que atuam em áreas não escolares, como gestão e coordenação de projetos educacionais em empresas, ONGs, editoras, instituições de treinamento e desenvolvimento, entre outros. ";
+    Titulo = "Pedagogia";
+    imagem = "<img src='img/Pedagogia.png' class='img-fluid' id='tm'>";
+    const el = document.querySelector('#egll');
+    el.style.cssText = 
+    'color: #fffff;';
+    document.getElementById("kigq").innerText = Perfil;
+    document.getElementById("zaba").innerText = Titulo;
+    document.getElementById("lxsx").innerHTML = imagem;
+    document.getElementById("kjni").innerHTML = Mercado;
+    document.getElementById("yrab").innerText = Curso;
 
 }
